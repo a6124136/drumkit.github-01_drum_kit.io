289dec9fe7edbff857bc8f7f3491cd95ef322575
a6124136 <110946547+a6124136@users.noreply.github.com>
2024-02-03 20:16:09 +0800

    Update drum.js
    remove desc

--- 01_drum_kit/js/drum.js
+++ 01_drum_kit/js/drum.js
@@ -1,5 +1,4 @@
-//範例是直接寫一般電腦預設的KeyCode透過 
-// 這邊可以更細節一點用querySelector 監聽整個HTML頁面
+//querySelector 監聽整個HTML頁面
 const html = document.querySelector('html');
 //設置一個常數html的去取得html的整個DOM (Document Object Module)文字物件模組
 //並且寫一個事件監聽器觸發條件為Keyup，鬆開按鍵時觸發e(element)也就是在主控台顯示該按鍵的所有內容物
@@ -8,15 +7,16 @@ const html = document.querySelector('html');
 //可以從主控台看出A=65 S=83 D=68 F=70 G=71 H=72 J=74 K=75 L=76 
 
 html.addEventListener("keydown", function (e) {
-    console.log(e.keyCode);
+//   console.log(e.keyCode);
   //以下函式操作按鍵撥放音樂 
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   //把所有audio的html標籤作為物件取到js內部，呼叫是以data-key="e.keyCode"的方式，也就是當前按什麼按鍵就出現什麼值
-  //${}為把變數或表達式引用為數值的寫法，勿忘此用法
+  //${}為把變數或表達式引用為數值的寫法
    if(audio) {
-    audio.currentTime = 0 //這個currentTime = 0 意思是按鍵刷新時間為0，不用等到音效播完就可以再播同一個音效
+    audio.currentTime = 0 //這個currentTime = 0 意思是音效刷新時間為0，不用等到音效播完就可以再播同一個音效
     audio.play() ;
 }
+
    //執行音樂檔audio的html，這邊的audio常數已經用document.querySelector取得個別的audio物件了(主控台看)
    //直接用執行涵式play()他就會依照當前按鍵傳進去的${e.keyCode}，決定要撥放哪一個音效檔
    //但是當按到不是播放音效的按鍵時就會報錯，所以要添加if判別，如果有按到audio才執行audio.play()
